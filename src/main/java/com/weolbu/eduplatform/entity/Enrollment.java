48686acbbec00ffac5919ee221ac246eeaeb9909
skgndi12 <skgndi123@naver.com>
2024-07-13 00:15:46 +0900

    refactor: rename Enrollment entity

--- src/main/java/com/weolbu/eduplatform/entity/Enrollment.java
+++ src/main/java/com/weolbu/eduplatform/entity/Enrollment.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Entollment {
+public class Enrollment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/weolbu/eduplatform/repository/EnrollmentRepository.java
+++ src/main/java/com/weolbu/eduplatform/repository/EnrollmentRepository.java
@@ -2,8 +2,8 @@ package com.weolbu.eduplatform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.weolbu.eduplatform.entity.Entollment;
+import com.weolbu.eduplatform.entity.Enrollment;
 
-public interface EnrollmentRepository extends JpaRepository<Entollment, Long> {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     long countByLectureId(Long lectureId);
 }
