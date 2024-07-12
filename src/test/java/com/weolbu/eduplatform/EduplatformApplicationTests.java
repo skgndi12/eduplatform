7584c4da2c4245d0ac04a513addbb33e73e5d6d6
skgndi12 <skgndi123@naver.com>
2024-07-12 22:21:30 +0900

    chore: init project

---
+++ src/test/java/com/weolbu/eduplatform/EduplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weolbu.eduplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
