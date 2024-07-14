a27161e1f204ac5805c615e1cf53a587789a3f1c
skgndi12 <skgndi123@naver.com>
2024-07-15 01:15:28 +0900

    feat: implement global exception handler

---
+++ src/main/java/com/weolbu/eduplatform/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.weolbu.eduplatform.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>("An error occured: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
