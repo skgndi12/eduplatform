76f571a5b83c894b784791f5e925e6ec60e1bbeb
skgndi12 <skgndi123@naver.com>
2024-07-15 01:16:50 +0900

    feat: implement  find all query  lectures

--- src/main/java/com/weolbu/eduplatform/repository/LectureRepository.java
+++ src/main/java/com/weolbu/eduplatform/repository/LectureRepository.java
@@ -1,7 +1,15 @@
 package com.weolbu.eduplatform.repository;
 
 import com.weolbu.eduplatform.entity.Lecture;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+    @Query("SELECT l FROM Lecture l LEFT JOIN l.enrollments e GROUP BY l ORDER BY COUNT(e) DESC")
+    Page<Lecture> findAllOrderByEnrollmentCountDesc(Pageable pageable);
+
+    @Query("SELECT l FROM Lecture l LEFT JOIN l.enrollments e GROUP BY l ORDER BY COUNT(e) * 1.0 / l.maxStudents DESC")
+    Page<Lecture> findAllOrderByEnrollmentRateDesc(Pageable pageable);
 }
