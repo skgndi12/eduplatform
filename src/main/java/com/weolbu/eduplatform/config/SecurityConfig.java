870ff010922b62d57e09aa2d67ddf5c1b32c6cf5
skgndi12 <skgndi123@naver.com>
2024-07-15 17:33:31 +0900

    feat: seperate security filter chain by env

--- src/main/java/com/weolbu/eduplatform/config/SecurityConfig.java
+++ src/main/java/com/weolbu/eduplatform/config/SecurityConfig.java
@@ -2,21 +2,36 @@ package com.weolbu.eduplatform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Profile("dev")
+    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf.disable()); // For simplicity in development. Be cautious in production.
+                        .anyRequest().permitAll() // 모든 요청에 대해 접근 허용
+                )
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .cors(cors -> cors.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    @Profile("prod")
+    public SecurityFilterChain prodSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().authenticated());
 
         return http.build();
     }
